76fa993aeed214d7ed343de2423c16f24d96aded
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-08-27 12:58:25 +0545

    the jikan fetch function addedd

--- script_01.js
+++ script_01.js
@@ -1,4 +1,24 @@
 // getting elements 
 const backBtn = document.getElementById("backBtn");
-const whatsNext = document.getElementById("whatsNext");
+const whatsNextBtn = document.getElementById("whatsNext");
 const nextBtn = document.getElementById("nextBtn");
+// the apis 
+var jikanUrl = "https://api.jikan.moe/v4/random/anime";
+
+function hikariGets() {
+    console.log("geting a anime.......!!!!!!");
+    fetch(jikanUrl)
+        .then(function(r){
+            return r.json();
+        })
+        .then(function(d){
+            console.log(d);
+            var anime = d.data;
+            posterImg.src = anime.images.jpg.large_image_url;
+            posterImg.style.display = "block";
+        })
+        .catch(function(e){
+            console.log("oops",e);
+        });
+}
+whatsNextBtn.addEventListener("click", hikariGets);
