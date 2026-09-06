7073dffef520c67240a286f70a64952669ffe73d
Shrestha <234707564+shrestha-01@users.noreply.github.com>
2026-09-06 09:51:25 +0545

    switched tmdb to discover endpoint for real random movies, fixed some typos causing issue as well

--- Backend/tmdb.php
+++ Backend/tmdb.php
@@ -1,10 +1,24 @@
 <?php
 require 'x.php';
 
-$movieId = rand(1, 500000);
-$url = "https://api.themoviedb.org/3/movie/" . $movieId . "?api_key=" . TMDB_KEY;
+$randomPage = rand(1, 500);
+$url = "https://api.themoviedb.org/3/discover/movie?api_key=" . TMDB_KEY .
+    "&sort_by=popularity.desc&page=" . $randomPage;
 
 $response = file_get_contents($url);
+$data = json_decode($response, true);
+
+if (!$data || empty($data['results'])) {
+    header('Content-Type: application/json');
+    echo json_encode(['success' => false, 'status_message' => 'no results on this page']);
+    exit;
+}
+
+$randomIndex = array_rand($data['results']);
+$movieId = $data['results'][$randomIndex]['id'];
+
+$detailUrl = "https://api.themoviedb.org/3/movie/" . $movieId . "?api_key=" . TMDB_KEY;
+$detailResponse = file_get_contents($detailUrl);
 
 header('Content-Type: application/json');
-echo $response;
+echo $detailResponse;
